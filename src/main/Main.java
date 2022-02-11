c4e13069898c69ce2957687bca6d479110860fc2
Álvaro Fernández <avunotfound@gmail.com>
2022-02-11 13:55:50 +0100

    De mi para ti

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,19 +3,40 @@ package main;
 import java.io.*;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        File fichero = new File("ficheiro");
+        File fichero = new File(askFileName());
         BufferedInputStream bin = createBufferInputStream(fichero);
-        byte[] hash = getFileHash(bin);
+        byte[] hash = getFileHash(bin, askAlgorithm());
         toHexString(hash);
     }
-
-    private static byte[] getFileHash(BufferedInputStream bin) {
+    private static String askAlgorithm()
+    {
+        String algorithm="SHA-256";
+        Scanner sc= new Scanner(System.in);
+        System.out.println("""
+                Seleccione el algoritmo que desea utilizar:
+                1.SHA-256
+                2.MD5
+                """);
+        if(sc.next().equals("2"))
+            algorithm="MD5";
+        return algorithm;
+    }
+    private static String askFileName()
+    {
+        String fileName;
+        System.out.println("introduzca el nombre del fichero");
+        Scanner sc= new Scanner(System.in);
+        fileName=sc.next();
+        return fileName;
+    }
+    private static byte[] getFileHash(BufferedInputStream bin, String algorithm) {
         byte[] hash=new byte[0];
         try {
-            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            MessageDigest md = MessageDigest.getInstance(algorithm);
             md.update(bin.readAllBytes());
             hash = md.digest();
         } catch (NoSuchAlgorithmException | IOException e) {
